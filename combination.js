433a12e041def4bdf0cb1abd49a3b29a3440aa7d
Sylvain Kalache <sylvain@kalache.fr>
2013-10-19 00:00:39 -0700

    got the 2 parts working, need to assemble them now

--- combination.js
+++ combination.js
@@ -1,33 +1,42 @@
-var pieces = [[2,1,5,4,2],
-              [2,2,2,4,1],
-              [2,3,3,4,3],
-              [4,4,4,4,4],
-              [3,5,5,5,5],
-             ];
+// var pieces = [[2,1,5,4,4],
+//               [2,1,2,4,4],
+//               [2,3,3,4,3],
+//               [4,3,4,2,4],
+//               [3,5,2,5,5],
+//              ];
+
+function move_one(grid) {
+    to_move_1 = grid[2][3];
+    to_move_2 = grid[2][4];
+
+    grid[2][3] = to_move_2;
+    grid[2][4] = to_move_1;
+
+    find_horizontal_combination(grid);
+    find_vertical_combination(grid);
+}
 
 function find_horizontal_combination(pieces) {
     // Finding array size
     array_hori_size = pieces.length;
     array_vert_size = pieces[0].length;
     // Go through columns
-    for (i = 0; i < array_vert_size; i++) {
+    for (i = 0; i < array_hori_size; i++) {
 	var count = 0, best_combination = 0;
 	var to_match = pieces[i][0];
 
 	// Go through lines
-	for (j = 0; j < array_hori_size; j++) {
+	for (j = 0; j < array_vert_size; j++) {
 	    if (pieces[i][j] == to_match) {
-		// Increase count
 		if (++count >= 3) {
 		    best_combination = count;
 		}
 	    } else {
-		// Reset count
-		var to_match = pieces[i][j];
-		count = 0;
+		count = 1;
 	    }
+	    var to_match = pieces[i][j];
 	}
-	console.log("Best match for line "+i+" :"+best_combination);
+	console.log("Best match for line "+i+": "+(best_combination));
     }
 }
 
@@ -38,7 +47,7 @@ function find_vertical_combination(pieces) {
     // Go through lines
     for (i = 0; i < array_vert_size; i++) {
 	var count = 0, best_combination = 0;
-	var to_match = pieces[i][0];
+	var to_match = pieces[0][i];
 
 	// Go through columns
 	for (j = 0; j < array_hori_size; j++) {
@@ -49,13 +58,12 @@ function find_vertical_combination(pieces) {
 		}
 	    } else {
 		// Reset count
-		var to_match = pieces[j][i];
-		count = 0;
+		count = 1;
 	    }
+	    var to_match = pieces[j][i];
 	}
-	console.log("Best match for column "+i+" :"+best_combination);
+	console.log("Best match for column "+i+": "+best_combination);
     }
 }
 
-find_horizontal_combination(pieces);
-find_vertical_combination(pieces);
+//move_one(pieces);
